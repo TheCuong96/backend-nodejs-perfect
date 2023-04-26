5c76e2c5f04fee66cfe27e85750e4184cb2e4080
thecuong1396 <cuongphan@coinmap.tech>
2023-04-27 01:13:57 +0700

    handle create data with mysql2

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,4 @@
+const connection = require("../config/databaseConnectPool");
 let getHomepage = async (req, res) => {
     res.render("home.ejs");
 };
@@ -5,9 +6,25 @@ let getSamplepage = async (req, res) => {
     res.render("sample.ejs"); // vì ở trên ta đã setup ở thử mục views rồi, nên ở đây ta chỉ cần gọi tên file ejs ra mà dùng thôi
 };
 
+// let postCreateUser = (req, res) => {
+//     let { name, email, city } = req.body;
+//     connection.query(
+//         `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
+//         [email, name, city],
+//         function (err, results) {
+//             console.log("results", results);
+//             return res.redirect("/");
+//         }
+//     );
+// };
+
 let postCreateUser = async (req, res) => {
-    console.log("req", req.body);
-    res.send("create user");
+    let { name, email, city } = req.body;
+    await connection.execute(
+        `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
+        [email, name, city]
+    );
+    return res.redirect("/");
 };
 module.exports = {
     getHomepage,
