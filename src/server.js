70456e5816fdd2d3324c3ba6d23a5aa5874dcece
thecuong1396 <cuongphan@coinmap.tech>
2023-04-26 08:40:14 +0700

    sử dụng docker, dbeaver, mysql2 connect with MySQL

--- src/server.js
+++ src/server.js
@@ -8,7 +8,20 @@ const port = process.env.PORT || 3000;
 // const hostname = process.env.HOSTNAME;
 
 configViewEngine(app);
+const mysql = require("mysql2");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 3307,
+    database: "hoidanit",
+    password: "123456",
+});
+
+connection.query("SELECT * FROM Users u", function (err, results, fiedls) {
+    console.log("results", results);
+    console.log("fiedls", fiedls);
+});
 app.use("/", routers);
 
 app.listen(port, () => {
