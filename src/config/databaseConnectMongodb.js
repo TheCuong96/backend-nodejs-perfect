f978c3c6d8ee646f4570ad3d0c25b456c1364941
thecuong1396 <cuongphan@coinmap.tech>
2023-04-27 06:42:34 +0700

    connection to mongodb

---
+++ src/config/databaseConnectMongodb.js
@@ -0,0 +1,21 @@
+const { log } = require("console");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbState = [
+    { value: 0, label: "disconnected" },
+    { value: 1, label: "connected" },
+    { value: 2, label: "connecting" },
+    { value: 3, label: "disconnecting" },
+];
+const connection = async () => {
+    try {
+        await mongoose.connect("mongodb://root:123456@localhost:27018/");
+        const state = Number(mongoose.connection.readyState);
+        console.log(dbState.find((f) => f.value == state).label, "to db"); // connected
+    } catch (error) {
+        console.log("connect error", error);
+    }
+};
+
+module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routers = require("./routes/web");
 
+const connection = require("./config/databaseConnectMongodb");
 const configViewEngine = require("./config/viewEngine");
 require("dotenv").config();
 const app = express();
@@ -10,7 +11,7 @@ const port = process.env.PORT || 3000;
 //2 thằng dưới đây dùng để compile code lại code js mà ta có thể hiểu và sử dụng được ở node mỗi khi có data được bắn qua lại giữa server và client
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+connection();
 configViewEngine(app);
 app.use("/", routers);
 
