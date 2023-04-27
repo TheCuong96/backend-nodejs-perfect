d75e3f40c78e31175e201f606b533324cc053d75
thecuong1396 <cuongphan@coinmap.tech>
2023-04-27 10:05:57 +0700

    submit edit with mongoose

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,32 +4,15 @@ const connection = require("../config/databaseConnectPool");
 const User = require("../models/user");
 
 let getHomepage = async (req, res) => {
-    let results = [];
+    let results = await User.find({});
     return res.render("home.ejs", { dataUser: results });
 };
 let getSamplepage = async (req, res) => {
     res.render("sample.ejs"); // vì ở trên ta đã setup ở thử mục views rồi, nên ở đây ta chỉ cần gọi tên file ejs ra mà dùng thôi
 };
 
-// let postCreateUser = (req, res) => {
-//     let { name, email, city } = req.body;
-//     connection.query(
-//         `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
-//         [email, name, city],
-//         function (err, results) {
-//             console.log("results", results);
-//             return res.redirect("/");
-//         }
-//     );
-// };
-
 let postCreateUser = async (req, res) => {
     let { name, email, city } = req.body;
-    // let [results, fields] = await connection.execute(
-    //     `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
-    //     [email, name, city]
-    // );
-    console.log("xxxxxxx", { name, email, city });
     await User.create({ name, email, city });
     return res.redirect("/");
 };
@@ -39,22 +22,13 @@ let getCreatePage = async (req, res) => {
 };
 
 let getEditPage = async (req, res) => {
-    // lấy data cần edit từ server bằng id
-    let id = req.params.id;
-    console.log("id", id);
-    let [user] = await connection.execute("Select * from Users where id = ?", [
-        id,
-    ]);
-    console.log("user", user[0]);
-    return res.render("edit.ejs", { dataUser: user[0] });
+    let { id } = req.params;
+    let user = await User.findById(id).exec();
+    return res.render("edit.ejs", { dataUser: user });
 };
 let postSubmitEditUser = async (req, res) => {
     // gửi data lên server để cập nhật lại data
-    let { name, email, city, id } = req.body;
-    await connection.execute(
-        "update Users set name = ? , email = ? , city= ? where id = ?",
-        [name, email, city, id]
-    );
+    await User.updateOne({ _id: req.body.id }, { ...req.body });
     return res.redirect("/");
 };
 
