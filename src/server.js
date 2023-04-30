ed428f3485b6429c556c7640b4c76f5b2f0d5b0a
thecuong1396 <cuongphan@coinmap.tech>
2023-05-01 02:04:15 +0700

    using mongodb driver để tạo và lấy dữ liệu

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const configViewEngine = require("./config/viewEngine");
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
 
+const { MongoClient } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -22,7 +24,22 @@ app.use("/v1/api/", routerAPI);
 
 (async () => {
     try {
-        await connection();
+        // using mongoose
+        // await connection();
+
+        // using mongodb
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const dbName = process.env.DB_NAME;
+        const client = new MongoClient(url);
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection("customers");
+        // collection.insertOne({ address: "hcm", email: "thecuong@gmail.com" });
+        // collection.insertOne({ address: [1, 2, 3] });
+        let a = await collection.findOne({ address: "hcm" });
+        console.log("find", a);
+        
+        console.log("Connected successfully to server");
         app.listen(port, () => {
             console.log(`backend zore listening on port ${port}`);
         });
