fab67b499ccd074a7d8e4ae8b482acebe9244b8f
thecuong1396 <cuongphan@coinmap.tech>
2023-05-02 13:03:02 +0700

    cách loại người dùng ra khỏi project

--- src/services/projectServices.js
+++ src/services/projectServices.js
@@ -4,11 +4,22 @@ const aqp = require("api-query-params");
 const createProjectService = async (data) => {
     try {
         if (data.type === "EMPTY-PROJECT") {
+            // dùng để tạo project
             return await Project.create(data);
         }
         if (data.type === "ADD-USERS") {
+            // dùng để thêm người dùng vào project
             let myProject = await Project.findById(data.projectId).exec();
             myProject.usersInfor.push(...data.userArr);
+            return await myProject.save();
+        }
+        if (data.type === "REMOVE-USERS") {
+            // dùng để xóa người dùng ra khỏi project, muốn xóa người dùng ra khỏi project chỉ cần ghi đè lại nó và loại nó ra khỏi data trong lúc ghi đè là được
+            let myProject = await Project.findById(data.projectId).exec();
+            console.log("myProject", myProject);
+            const aaa = myProject.usersInfor.pull(...data.userArr);
+            console.log("aaa", aaa);
+
             return await myProject.save();
         }
     } catch (error) {
